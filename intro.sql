e1d62862afbfa9d0cdeaa4b99456a3f517a4b087
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-08-08 01:50:41 +0600

    Adding Constraints in a table

--- intro.sql
+++ intro.sql
@@ -1,24 +1,58 @@
 -- Active: 1691433021717@@127.0.0.1@5432@test1
 
 -- Create DATABASE
+
 -- CREATE DATABASE test2;
 
 -- RENAME DATABASE
+
 -- ALTER DATABASE test2 RENAME TO test1;
 
 -- Delete DATABASE
+
 -- DROP DATABASE test1;
 
 -- Create a Student TABLE
+
 -- CREATE TABLE student(
+
 --     student_id INT,
+
 --     first_name VARCHAR(50),
+
 --     last_name VARCHAR(50),
+
 --     cgpa NUMERIC(1,2)
+
 -- )
 
 -- Update a table name
+
 -- ALTER TABLE student RENAME TO learners;
 
 -- Delete a TABLE
+
 -- DROP TABLE learners;
+
+-- Create a TABLE with Constraints
+
+-- CREATE TABLE "users2"(
+
+--     user_id SERIAL,
+
+--     user_name VARCHAR(255)UNIQUE NOT NULL,
+
+--     email VARCHAR(255) UNIQUE NOT NULL,
+
+--     PRIMARY KEY(user_id, user_name)
+
+--     age DEFAULT 18
+
+-- )
+
+-- insert value
+
+-- INSERT INTO users2 values (1,'abc','abc@gmail.com');
+
+-- Table show
+-- SELECT * FROM users2;
