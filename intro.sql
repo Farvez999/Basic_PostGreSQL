a32c8722a4ed0130df181cf2efe12cf65a041768
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-08-08 01:30:16 +0600

    first commit

---
+++ intro.sql
@@ -0,0 +1,24 @@
+-- Active: 1691433021717@@127.0.0.1@5432@test1
+
+-- Create DATABASE
+-- CREATE DATABASE test2;
+
+-- RENAME DATABASE
+-- ALTER DATABASE test2 RENAME TO test1;
+
+-- Delete DATABASE
+-- DROP DATABASE test1;
+
+-- Create a Student TABLE
+-- CREATE TABLE student(
+--     student_id INT,
+--     first_name VARCHAR(50),
+--     last_name VARCHAR(50),
+--     cgpa NUMERIC(1,2)
+-- )
+
+-- Update a table name
+-- ALTER TABLE student RENAME TO learners;
+
+-- Delete a TABLE
+-- DROP TABLE learners;
