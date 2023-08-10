7c840077d39078f21b6d559891a9406e4af9c8a7
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-08-10 23:28:48 +0600

    Foreign Key Constraint And Data Integrity

--- intro.sql
+++ intro.sql
@@ -55,6 +55,56 @@
 -- INSERT INTO users2 values (1,'abc','abc@gmail.com');
 
 -- Table show
+<<<<<<< HEAD
+
+-- SELECT * FROM users2;
+
+-- INSERT INTO users2 (user_id, user_name, email) VALUES (2, 'Farvez', 'farvez@gmail.com')
+
+-- SELECT * FROM users2;
+
+-- Multiple values INSERT
+
+-- INSERT INTO users2 (user_id, user_name, email) VALUES (3, 'Farvez1', 'fa1@gmail.com'), (4, 'Farvez2', 'fa2@gmail.com');
+
+-- Delete all rows in a table without deleting the table
+
+-- TRUNCATE TABLE users2;
+
+-- Module 31
+
+-- Foreign Key Constraint And Data Integrity
+
+-- Department TABLE
+
+-- Each Department has many Employee
+
+CREATE TABLE
+    Department(
+        deptID SERIAL PRIMARY KEY,
+        deptName VARCHAR(50)
+    );
+
+INSERT INTO Department VALUES(1,'IT');
+DELETE FROM Department WHERE deptID = 1;
+SELECT * FROM Department;
+
+-- Employee TABLE
+
+-- Each Employee belongs to department
+
+CREATE TABLE
+    Employee(
+        empID SERIAL PRIMARY KEY,
+        empNAme VARCHAR(50) NOT NULL,
+        departmentID INT,
+        CONSTRAINT fk_constraint_dept FOREIGN KEY (departmentID) REFERENCES Department(deptID)
+    );
+
+INSERT into Employee VALUES(1, 'Farvez Hossen', 1);
+DELETE FROM Employee WHERE empID = 1;
+SELECT * From Employee;
+=======
 -- SELECT * FROM users2;
 
 -- Alter Table 
@@ -62,3 +112,4 @@
 -- rename a colum , set DEFULT value for a colum 
 -- add CONSTRAINT to a colum, drop CONSTRAINT for a colum
 -- table rename
+>>>>>>> 342864a29964a07e4415d8eb4a4d4d108a02659a
